3eb0f5196dc49ef8bd64b352faeec4b6e5866012
Dan Farrell <dfarrell@spscommerce.com>
2018-02-08 13:50:48 -0600

    a solution I hope you might agree is more elegant

--- python.py
+++ python.py
@@ -1,16 +1,18 @@
-ssm = boto3.client('ssm')
-res = ssm.get_parameters_by_path(
-    Path=os.environ.get('SSM_PATH'),
-    WithDecryption=True
-)
-parameters = res['Parameters']
+import boto3
 
-while res.get('NextToken'):
+more = nil
+args = dict(Path=os.environ.get('SSM_PATH'), WithDecryption=True)
+ssm = boto3.client('ssm')
+parameters = []
+while more != false:
+    if more:
+        args["NextToken"] = more        
     res = ssm.get_parameters_by_path(
         Path=os.environ.get('SSM_PATH'),
-        WithDecryption=True,
-        NextToken=res.get('NextToken')
+        WithDecryption=True
     )
     parameters.extend(res['Parameters'])
+    more = res.get("NextToken", false)
+
 for secret in parameters:
     os.environ[secret.get('Name').split('/')[-1]] = secret.get('Value')
