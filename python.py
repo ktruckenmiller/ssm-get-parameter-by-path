c5f917bb2f67127ee1a1875ab1b7faae828636df
ktruckenmiller <kmtruckenmiller@gmail.com>
2018-05-12 09:50:17 -0500

    updated

--- python.py
+++ python.py
@@ -1,18 +1,18 @@
 import boto3, os
+'''
+    Sets your os environ variables to whatever is at the current SSM Path
 
-more = None
-args = dict(Path=os.environ.get('SSM_PATH'), WithDecryption=True)
+    /bin/sh# SSM_PATH=/ python python.py
+
+    will print out your current os environ variables based on whatever is at the root ssm path
+'''
+
 ssm = boto3.client('ssm')
-parameters = []
-while more != False:
-    if more:
-        args["NextToken"] = more        
-    res = ssm.get_parameters_by_path(
-        Path=os.environ.get('SSM_PATH'),
-        WithDecryption=True
-    )
-    parameters.extend(res['Parameters'])
-    more = res.get("NextToken", false)
-
-for secret in parameters:
-    os.environ[secret.get('Name').split('/')[-1]] = secret.get('Value')
+paginator = ssm.get_paginator('get_parameters_by_path')
+iterator = paginator.paginate(Path=os.environ.get('SSM_PATH'), WithDecryption=True)
+params = []
+for page in iterator:
+    params.extend(page['Parameters'])
+    for param in page.get('Parameters', []):
+        os.environ[param.get('Name').split('/')[-1]] = param.get('Value')
+print(os.environ)
